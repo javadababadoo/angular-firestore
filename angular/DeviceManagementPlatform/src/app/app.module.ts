9efa4ba1b41a87d2238021f4c39054c65faee235
Jaime Zapata <jezg1992@hotmail.com>
2018-02-09 17:27:38 -0500

    add Angular material

--- angular/DeviceManagementPlatform/src/app/app.module.ts
+++ angular/DeviceManagementPlatform/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MaterialModule,
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ angular/DeviceManagementPlatform/src/app/material.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatSnackBarModule,
+  MatGridListModule,
+  MatDialogModule,
+  MatSelectModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatSelectModule
+  ],
+})
+export class MaterialModule {
+
+}
